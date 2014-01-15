970832136b88c480120ef4e9a51a6f437c71bd2e
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-01-15 12:16:34 -0500

    Removes rendered indexes from the repo. Gives a list route for graphics.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
+from glob import glob
+
 from flask import Flask, render_template
 
 import app_config
@@ -12,14 +14,27 @@ app.register_blueprint(static.static)
 
 app.jinja_env.filters['urlencode'] = urlencode_filter
 
-# Example application views
+@app.route('/')
+def _graphics_list():
+    context = make_context()
+    context['graphics'] = []
+
+    graphics = glob('www/graphics/*/child.html')
+    for graphic in graphics:
+        context['graphics'].append(graphic.split('www/graphics/')[1].split('/child.html')[0])
+
+    context['graphics_count'] = len(context['graphics'])
+
+    return render_template('index.html', **context)
+
 @app.route('/graphics/<slug>/')
-def _render_parent(slug):
+def _graphics_detail(slug):
     """
     Renders a parent.html index with child.html embedded as iframe.
     """
 
-    context = {'slug': slug}
+    context = make_context()
+    context['slug'] = slug
     context['domain'] = app_config.S3_BASE_URL
 
     return render_template('parent.html', **context)
