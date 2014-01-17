aaba59acf194408390cf622d9aca0b22eb32e8a8
Alyson Hurt <ahurt@npr.org>
2014-01-17 12:07:32 -0500

    scoring note at bottom of quiz

--- www/graphics/quiz-china-driving/graphic.js
+++ www/graphics/quiz-china-driving/graphic.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     var $quiz = $('#quiz');
+    var $results = $('#results');
+    var num_correct = 0;
 
     $quiz.find('li strong').click(function(){
         var $q = $(this).parents('.question');
@@ -12,9 +14,27 @@ $(document).ready(function() {
 
         if (got_it_right) {
             $(this).prepend('<b>RIGHT!</b> ');
+            num_correct++;
+            console.log(num_correct);
+            switch(num_correct) {
+                case 1:
+                    $results.html('You answered <strong>' + num_correct + '</strong> question correctly &mdash; not good enough to drive in China.');
+                    break;
+                case 9:
+                    $results.html('You answered <strong>' + num_correct + '</strong> questions correctly. Congratulations! You\'ve passed the test!');
+                    break;
+                case 10:
+                    $results.html('You answered <strong>' + num_correct + '</strong> questions correctly. Amazing! You\'ve aced the test!');
+                    break;
+                default:
+                    $results.html('You answered <strong>' + num_correct + '</strong> questions correctly &mdash; not good enough to drive in China.');
+                    break;
+            }
         } else {
             $(this).prepend('<b>WRONG!</b> ');
         }
+ 
+        console.log($results.text());
 
         $b.each(function(v,k) {
             $(this).unbind('click');
