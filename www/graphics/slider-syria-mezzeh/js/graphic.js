d01b6f27b272ad665ae4148a4f1d5360ad454ed7
Alyson Hurt <ahurt@npr.org>
2014-01-30 14:55:36 -0500

    update responsive slider

--- www/graphics/slider-syria-mezzeh/js/graphic.js
+++ www/graphics/slider-syria-mezzeh/js/graphic.js
@@ -9,12 +9,24 @@ function render(width) {
     var new_width = $ba.width();
     var ratio = new_width / orig_width;
     var new_height = orig_height * ratio;
-
+    
+    // reset old slider
+    $('.ui-draggable').remove();
+    $('.balinks').remove();
+    $('div[style]').removeAttr('style');
+    $('img[style]').removeAttr('style');
+    $('img').each( function(i) {
+        if ($(this).attr('src') == './js/lib/lt-small.png' ||
+            $(this).attr('src') == './js/lib/rt-small.png') {
+            $(this).remove();
+        }
+    });
+    
+    // create new slider
     $.each($ba_img_before, function(k,v) {
         $(v).width(new_width);
         $(v).height(new_height);
     });
-
     $('#before-after-1').beforeAfter( { beforeLinkText: '2004', afterLinkText: '2011' } );
 
     // update responsive iframe
