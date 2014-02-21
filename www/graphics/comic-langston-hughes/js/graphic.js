818be8e1f408bee0594389fa73ce456c362cf189
Alyson Hurt <ahurt@npr.org>
2014-02-21 16:01:01 -0500

    tweak img onload

--- www/graphics/comic-langston-hughes/js/graphic.js
+++ www/graphics/comic-langston-hughes/js/graphic.js
@@ -1,33 +1,25 @@
 var num_panels;
 
-/*
- * TODO: draw your graphic
- */
 function render(width) {
-    if (width >= 480) {
+    if (width >= 480) { // sub in desktop version
         for (var i = 0; i < num_panels; i++) {
             $('#panel' + (i + 1)).attr('src', '../../assets/comic-langston-hughes/panel-' + (i + 1) + '.jpg');
-            $('#panel' + (i + 1)).load(function() {
-                sendHeightToParent();
-            })
         }
-    } else {
+    } else { // use mobile version
         for (var i = 0; i < num_panels; i++) {
             $('#panel' + (i + 1)).attr('src', '../../assets/comic-langston-hughes/panel-' + (i + 1) + '-mobile.jpg');
-            $('#panel' + (i + 1)).load(function() {
-                sendHeightToParent();
-            })
         }
     }
 }
 
-/*
- * NB: Use window.load instead of document.ready
- * to ensure all images have loaded
- */
-$(window).load(function() {
+$(document).ready(function() {
     num_panels = $('#graphic img').length;
-    console.log(num_panels);
+
+    for (var i = 0; i < num_panels; i++) {
+        $('#panel' + (i + 1)).load(function() {
+            sendHeightToParent();
+        })
+    }
 
     setupResponsiveChild({
         renderCallback: render
