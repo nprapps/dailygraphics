eb7dc87c67687f9d8a7a249087468df713bce534
Alyson Hurt <ahurt@npr.org>
2014-03-24 18:25:56 -0400

    quiz edits

--- www/graphics/quiz-economy/js/graphic.js
+++ www/graphics/quiz-economy/js/graphic.js
@@ -1,23 +1,73 @@
-var colors = {
-    'red1': '#6C2315', 'red2': '#A23520', 'red3': '#D8472B', 'red4': '#E27560', 'red5': '#ECA395', 'red6': '#F5D1CA',
-    'orange1': '#714616', 'orange2': '#AA6A21', 'orange3': '#E38D2C', 'orange4': '#EAAA61', 'orange5': '#F1C696', 'orange6': '#F8E2CA',
-    'yellow1': '#77631B', 'yellow2': '#B39429', 'yellow3': '#EFC637', 'yellow4': '#F3D469', 'yellow5': '#F7E39B', 'yellow6': '#FBF1CD',
-    'teal1': '#0B403F', 'teal2': '#11605E', 'teal3': '#17807E', 'teal4': '#51A09E', 'teal5': '#8BC0BF', 'teal6': '#C5DFDF',
-    'blue1': '#28556F', 'blue2': '#3D7FA6', 'blue3': '#51AADE', 'blue4': '#7DBFE6', 'blue5': '#A8D5EF', 'blue6': '#D3EAF7'
-};
+$(document).ready(function() {
+    var $quiz = $('#quiz');
+    var $results = $('#results');
+    var $scoreboard = $('#scoreboard');
+    var $tiebreaker = $quiz.find('.question.tiebreaker');
 
-/*
- * TODO: draw your graphic
- */
-function render(width) {
-}
+    var current_question = 0; // 0-indexed
+    var num_taken = 0;
+    var num_questions = $quiz.find('div.question').length - 1;
+    var total_mattress = 0;
+    var total_stocks = 0;
+    
+    $tiebreaker.hide();
+    $results.hide();
+    
+    $quiz.find('.question:eq(' + current_question + ')').show();
 
-/*
- * NB: Use window.load instead of document.ready
- * to ensure all images have loaded
- */
+    $quiz.find('li strong').click(function(){
+        var $q = $(this).parents('.question');
+        var $a = $(this).parent('li');
+        var $b = $q.find('strong');
+        var category;
+        
+        if ($a.hasClass('stocks')) {
+            total_stocks++;
+        } else if ($a.hasClass('mattress')) {
+            total_mattress++;
+        }
+ 
+        $q.addClass('answered');
+        $a.addClass('correct').siblings('li').addClass('incorrect');
+        num_taken++
+
+        $b.each(function(v,k) {
+            $(this).unbind('click');
+        });
+ 
+        console.log(total_mattress, total_stocks);
+        if (num_taken == num_questions) {
+            if (total_mattress == total_stocks) {
+                $q.hide();
+                $q.next('.question').fadeIn();
+                num_questions++;
+            } else if (total_mattress != total_stocks) {
+                var winning_category;
+                if (total_mattress > total_stocks) {
+                    winning_category = 'mattress';
+                } else {
+                    winning_category = 'stocks';
+                }
+            
+                $q.hide();
+                $results.fadeIn();
+                $('#results').find('.' + winning_category).show();
+                $scoreboard.find('.' + winning_category).addClass('winner');
+            }
+        } else {
+            // show the next question
+            $q.hide();
+            $q.next('.question').fadeIn();
+        }
+        
+        // update the scoreboard
+        $scoreboard.find('.mattress').find('i').text(total_mattress);
+        $scoreboard.find('.stocks').find('i').text(total_stocks);
+        
+        sendHeightToParent();
+    });
+});
+
 $(window).load(function() {
-    setupResponsiveChild({
-        renderCallback: render
-    });
-})
+    setupResponsiveChild();
+});
