f87e671b05f85b831b6ad49b175b8a1b1a6d1106
Alyson Hurt <ahurt@npr.org>
2014-04-17 17:34:48 -0400

    slopegraph!

--- www/graphics/child-mortality/js/graphic.js
+++ www/graphics/child-mortality/js/graphic.js
@@ -1,8 +1,13 @@
 var $graphic;
+var graphic_data;
+var graphic_data_url = 'data.csv';
+var IS_MOBILE;
 var mobile_threshold = 480;
+var num_ticks;
 var pymChild = null;
-var height_ratio = 9;
-var width_ratio = 16;
+var min_height = 600;
+var height_ratio = 3;
+var width_ratio = 4;
 
 var colors = {
     'red1': '#6C2315', 'red2': '#A23520', 'red3': '#D8472B', 'red4': '#E27560', 'red5': '#ECA395', 'red6': '#F5D1CA',
@@ -11,19 +16,20 @@ var colors = {
     'teal1': '#0B403F', 'teal2': '#11605E', 'teal3': '#17807E', 'teal4': '#51A09E', 'teal5': '#8BC0BF', 'teal6': '#C5DFDF',
     'blue1': '#28556F', 'blue2': '#3D7FA6', 'blue3': '#51AADE', 'blue4': '#7DBFE6', 'blue5': '#A8D5EF', 'blue6': '#D3EAF7'
 };
-var graphic_data = {
-    "world": { "title": "World", "1990": 90, "2012": 48 },
-    "developed": { "title": "Developed regions", "1990": 15, "2012": 6 },
-    "developing": { "title": "Developing regions", "1990": 99, "2012": 53 },
-    "northern_africa": { "title": "Northern Africa", "1990": 73, "2012": 22 },
-    "sub_saharan_africa": { "title": "Sub-Saharan Africa", "1990": 177, "2012": 98 },
-    "latin_america": { "title": "Latin America and the Caribbean", "1990": 54, "2012": 19 },
-    "caucasus": { "title": "Caucasus and Central Asia", "1990": 73, "2012": 36 },
-    "eastern_asia": { "title": "Eastern Asia", "1990": 53, "2012": 14 },
-    "southern_asia": { "title": "Southern Asia", "1990": 126, "2012": 58 },
-    "southeast_asia": { "title": "South-eastern Asia", "1990": 71, "2012": 30 },
-    "western_asia": { "title": "Western Asia", "1990": 65, "2012": 25 },
-    "oceania": { "title": "Oceania", "1990": 74, "2012": 55 }
+
+var labels = {
+    'world': { 'full': 'World', 'short': 'World' },
+    'developed': { 'full': 'Developed regions', 'short': 'Developed' },
+    'developing': { 'full': 'Developing regions', 'short': 'Developing' },
+    'n_africa': { 'full': 'Northern Africa', 'short': 'N. Africa' },
+    'sub_africa': { 'full': 'Sub-Saharan Africa', 'short': 'Sub-Saharan' },
+    'lat_america': { 'full': 'Latin America', 'short': 'Lat. America' },
+    'c_asia': { 'full': 'Central Asia', 'short': 'Central Asia' },
+    'e_asia': { 'full': 'Eastern Asia', 'short': 'E. Asia' },
+    's_asia': { 'full': 'Southern Asia', 'short': 'S. Asia' },
+    'se_asia': { 'full': 'Southeastern Asia', 'short': 'S.E. Asia' },
+    'w_asia': { 'full': 'Western Asia', 'short': 'W. Asia' },
+    'oceania': { 'full': 'Oceania', 'short': 'Oceania' }
 };
 
 
@@ -31,15 +37,26 @@ var graphic_data = {
  * Render the graphic
  */
 function render(width) {
-    // TODO: draw your graphic
     if (Modernizr.svg) {
-        var margin = {top: 10, right: 15, bottom: 25, left: 35};
-        var width = width - margin.left - margin.right;
-        var height = Math.ceil((width * height_ratio) / width_ratio) - margin.top - margin.bottom;
-        var num_ticks = 13;
+        var margin = {top: 27, right: 150, bottom: 25, left: 150};
+
         if (width < mobile_threshold) {
             num_ticks = 5;
+            margin.left = 110;
+            margin.right = 110;
+            IS_MOBILE = true;
+        } else {
+            num_ticks = 10;
+            margin.left = 150;
+            margin.right = 150;
+            IS_MOBILE = false;
         }
+
+        var width = width - margin.left - margin.right;
+//        var height = Math.ceil((width * height_ratio) / width_ratio) - margin.top - margin.bottom;
+//        if (height < min_height) {
+            height = min_height - margin.top - margin.bottom;
+//        }
 
         // clear out existing graphics
         $graphic.empty();
@@ -52,37 +69,33 @@ function render(width) {
         
         var xAxis = d3.svg.axis()
             .scale(x)
-            .orient("bottom")
+            .orient("top")
+            .tickValues([graphic_data[0].year, graphic_data[1].year])
             .tickFormat(function(d,i) {
-                if (width <= mobile_threshold) {
-                    var fmt = d3.time.format('%y');
-                    return '\u2019' + fmt(d);
-                } else {
-                    var fmt = d3.time.format('%Y');
-                    return fmt(d);
-                }
+                var fmt = d3.time.format('%Y');
+                return fmt(d);
+            });
+
+        var xAxis_bottom = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickValues([graphic_data[0].year, graphic_data[1].year])
+            .tickFormat(function(d) {
+                var fmt = d3.time.format('%Y');
+                return fmt(d);
             });
             
-        var x_axis_grid = function() { return xAxis; }
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient('left')
-            .ticks(num_ticks);
-        
-        var y_axis_grid = function() { return yAxis; }
-        
         var line = d3.svg.line()
-            .x(function(d) { return x(d.date); })
+            .x(function(d) { return x(d.year); })
             .y(function(d) { return y(d.amt); });
         
         // parse data into columns
         var lines = {};
         for (var column in graphic_data[0]) {
-            if (column == 'date') continue;
+            if (column == 'year') continue;
             lines[column] = graphic_data.map(function(d) {
                 return { 
-                    'date': d.date, 
+                    'year': d.year, 
                     'amt': d[column]
                 };
             });
@@ -94,60 +107,122 @@ function render(width) {
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         
-        x.domain(d3.extent(graphic_data, function(d) { return d.date; }));
+        x.domain(d3.extent(graphic_data, function(d) { return d.year; }));
 
-        y.domain([
-            d3.min(d3.entries(lines), function(c) { 
-                return d3.min(c.value, function(v) { 
-                    var n = v.amt;
-                    return Math.floor(n);
-//                    return Math.floor(n/10) * 10; // round to next 10
-                }); 
-            }),
+        y.domain([0,
             d3.max(d3.entries(lines), function(c) { 
                 return d3.max(c.value, function(v) { 
                     var n = v.amt;
                     return Math.ceil(n);
-//                    return Math.ceil(n/10) * 10; // round to next 10
+                    return Math.ceil(n/10) * 10; // round to next 10
                 }); 
             })
         ]);
         
         svg.append('g')
             .attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + height + ')')
+            .attr('transform', 'translate(0,-5)')
             .call(xAxis);
+
+        svg.append('g')
+            .attr('class', 'x axis bottom')
+            .attr('transform', 'translate(0,' + (height + 5) + ')')
+            .call(xAxis_bottom);
         
-        svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis);
-
-        svg.append('g')
-            .attr('class', 'x grid')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(x_axis_grid()
-                .tickSize(-height, 0, 0)
-                .tickFormat('')
-            );
-
-        svg.append('g')
-            .attr('class', 'y grid')
-            .call(y_axis_grid()
-                .tickSize(-width, 0, 0)
-                .tickFormat('')
-            );
-
         svg.append('g').selectAll('path')
             .data(d3.entries(lines))
             .enter()
             .append('path')
                 .attr('class', function(d, i) {
-                    return 'line line-' + i;
+                    return 'line line-' + i + ' ' + d.key;
                 })
                 .attr('d', function(d) {
                     return line(d.value);
                 });
 
+        svg.append('g')
+            .attr('class', 'value begin')
+            .selectAll('text')
+                .data(d3.entries(lines))
+            .enter().append('text')
+                .attr('x', function(d, i) { 
+                    return x(d['value'][0]['year']);
+                })
+                .attr('y', function(d) { 
+                    var ypos = y(d['value'][0]['amt']);
+                    if (d.key == 'oceania') {
+                        ypos -= 12;
+                    }
+                    if (d.key == 'n_africa') {
+                        ypos -= 3;
+                    }
+                    if (d.key == 'c_asia') {
+                        ypos += 7;
+                    }
+                    if (d.key == 'e_asia') {
+                        ypos += 10;
+                    }
+                    if (d.key == 'se_asia') {
+                        ypos += 11;
+                    }
+                    if (d.key == 'w_asia') {
+                        ypos += 2;
+                    }
+                    return ypos;
+                })
+                .attr('dx', -6)
+                .attr('dy', 4)
+                .attr('text-anchor', 'end')
+                .attr('class', function(d) { return 'l-' + d.key; })
+                .text(function(d) { 
+                    if (IS_MOBILE) {
+                        return labels[d.key]['short'] + ' - ' + d['value'][0]['amt'];
+                    } else {
+                        return labels[d.key]['full'] + ' - ' + d['value'][0]['amt'];
+                    }
+                });
+
+        svg.append('g')
+            .attr('class', 'value end')
+            .selectAll('text')
+                .data(d3.entries(lines))
+            .enter().append('text')
+                .attr('x', function(d, i) { 
+                    return x(d['value'][1]['year']);
+                })
+                .attr('y', function(d) { 
+                    var ypos = y(d['value'][1]['amt']);
+                    if (d.key == 's_asia') {
+                        ypos -= 3;
+                    }
+                    if (d.key == 'developing') {
+                        ypos += 7;
+                    }
+                    if (d.key == 'world') {
+                        ypos += 5;
+                    }
+                    if (d.key == 'n_africa') {
+                        ypos += 4;
+                    }
+                    if (d.key == 'lat_america') {
+                        ypos += 8;
+                    }
+                    if (d.key == 'e_asia') {
+                        ypos += 5;
+                    }
+                    return ypos;
+                })
+                .attr('dx', 6)
+                .attr('dy', 4)
+                .attr('text-anchor', 'start')
+                .attr('class', function(d) { return 'l-' + d.key; })
+                .text(function(d) { 
+                    if (IS_MOBILE) {
+                        return d['value'][1]['amt'] + ' - ' + labels[d.key]['short'];
+                    } else {
+                        return d['value'][1]['amt'] + ' - ' + labels[d.key]['full'];
+                    }
+                });
     }
     
     if (pymChild) {
@@ -161,11 +236,20 @@ function render(width) {
  */
 $(window).load(function() {
     $graphic = $('#graphic');
+    if (Modernizr.svg) {
+        d3.csv(graphic_data_url, function(error, data) {
+            graphic_data = data;
 
-    console.log(graphic_data);
+            graphic_data.forEach(function(d) {
+                d.year = d3.time.format('%Y').parse(d.year);
+            });
 
-    // responsive iframe
-    var pymChild = new pym.Child({
-        renderCallback: render
-    });
+            pymChild = new pym.Child({
+                renderCallback: render
+            });
+        });
+    } else {
+        // responsive iframe
+        pymChild = new pym.Child();
+    }
 })
