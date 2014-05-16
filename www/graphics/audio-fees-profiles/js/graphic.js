99ba39dfca76ea0f53ea7d20b902e4875e12e3c2
Alyson Hurt <ahurt@npr.org>
2014-05-16 16:04:23 -0400

    audio-fees-profiles: get jplayer (sorta) working

--- www/graphics/audio-fees-profiles/js/graphic.js
+++ www/graphics/audio-fees-profiles/js/graphic.js
@@ -1,4 +1,10 @@
+var $players = null;
+var $current_player = null;
+var $play_buttons = null;
+var $player_progress = null;
+
 var pymChild = null;
+var num_people = 7;
 
 var colors = {
     'red1': '#6C2315', 'red2': '#A23520', 'red3': '#D8472B', 'red4': '#E27560', 'red5': '#ECA395', 'red6': '#F5D1CA',
@@ -9,22 +15,74 @@ var colors = {
 };
 
 /*
- * Render the graphic
- */
-function render(width) {
-    // TODO: draw your graphic
-    
-    if (pymChild) {
-        pymChild.sendHeightToParent();
-    }
-}
-
-/*
  * NB: Use window.load instead of document.ready
  * to ensure all images have loaded
  */
 $(window).load(function() {
-    pymChild = new pym.Child({
-        renderCallback: render
-    });
+    $players = $('.jp-jplayer');
+    $play_buttons = $('.jp-play');
+    $player_progress = $('.jp-progress-container');
+    
+    console.log($play_buttons);
+
+    /*
+     * Audio
+     */
+    // On mobile browers we load everything first, because otherwise we require two clicks to play
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+        $players.each(function() {
+            $(this).jPlayer({
+                ready: function() {
+                    $(this).jPlayer('setMedia', {
+                        mp3: $(this).data('mp3'),
+                        oga: $(this).data('ogg')
+                    }).jPlayer('pause');
+                },
+                play: function() {
+                    $(this).jPlayer('pauseOthers');
+                },
+                preload: "none",
+                swfPath: 'js',
+                supplied: 'mp3, oga',
+                cssSelectorAncestor: $(this).data('selector')
+            });
+        });
+    // On other browsers we load incrementally, because FF and Safari won't allow 7+ jplayers on a page
+    } else {
+        function init_audio() {
+            $current_player.jPlayer({
+                ready: function() {
+                    $current_player.jPlayer('setMedia', {
+                        mp3: $(this).data('mp3'),
+                        oga: $(this).data('ogg')
+                    }).jPlayer('play');
+                },
+                preload: 'none',
+                swfPath: 'js',
+                supplied: 'mp3, oga',
+                cssSelectorAncestor: $current_player.data('selector')
+            });
+        }
+
+        $play_buttons.click(function() {
+            var $parent_player = $(this).parents('.jp-audio').prev('.jp-jplayer');
+
+            if ($current_player && $parent_player.attr('id') == $current_player.attr('id')) {
+                return true;
+            }
+
+            if ($current_player) {
+                $current_player.jPlayer('destroy');
+            }
+
+            $current_player = $parent_player;
+            init_audio();
+
+            return true;
+        });
+    }
+
+
+    // responsive iframe
+    pymChild = new pym.Child({ });
 })
