afce175cf30cf4ad390fe0a83088cbe0128f3f54
Alyson Hurt <ahurt@npr.org>
2014-05-21 14:16:14 -0400

    new: table-gallup-positive-scores

---
+++ www/graphics/table-gallup-positive-scores/graphic_config.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+COPY_GOOGLE_DOC_KEY = '1o6rLEuOXuQ8_Asw4I-Rghw6x3aJoUaK9GsItP5fJUZ8'
---
+++ www/graphics/table-gallup-positive-scores/js/graphic.js
@@ -0,0 +1,30 @@
+var pymChild = null;
+
+var colors = {
+    'red1': '#6C2315', 'red2': '#A23520', 'red3': '#D8472B', 'red4': '#E27560', 'red5': '#ECA395', 'red6': '#F5D1CA',
+    'orange1': '#714616', 'orange2': '#AA6A21', 'orange3': '#E38D2C', 'orange4': '#EAAA61', 'orange5': '#F1C696', 'orange6': '#F8E2CA',
+    'yellow1': '#77631B', 'yellow2': '#B39429', 'yellow3': '#EFC637', 'yellow4': '#F3D469', 'yellow5': '#F7E39B', 'yellow6': '#FBF1CD',
+    'teal1': '#0B403F', 'teal2': '#11605E', 'teal3': '#17807E', 'teal4': '#51A09E', 'teal5': '#8BC0BF', 'teal6': '#C5DFDF',
+    'blue1': '#28556F', 'blue2': '#3D7FA6', 'blue3': '#51AADE', 'blue4': '#7DBFE6', 'blue5': '#A8D5EF', 'blue6': '#D3EAF7'
+};
+
+/*
+ * Render the graphic
+ */
+function render(width) {
+    // TODO: draw your graphic
+    
+    if (pymChild) {
+        pymChild.sendHeightToParent();
+    }
+}
+
+/*
+ * NB: Use window.load instead of document.ready
+ * to ensure all images have loaded
+ */
+$(window).load(function() {
+    pymChild = new pym.Child({
+        renderCallback: render
+    });
+})
