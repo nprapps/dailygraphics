7d7c4e5bf0e06729a2d7bded43c0b63ec6bf8612
brentajones <bjones@bjones.net>
2014-06-27 12:52:23 -0500

    update targets

--- app_config.py
+++ app_config.py
@@ -18,8 +18,8 @@ ASSETS_SLUG = PROJECT_SLUG
 
 # The name of the repository containing the source
 REPOSITORY_NAME = 'dailygraphics'
-REPOSITORY_URL = 'git@github.com:nprapps/%s.git' % REPOSITORY_NAME
-REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
+REPOSITORY_URL = 'git@github.com:stlpublicradio/%s.git' % REPOSITORY_NAME
+# REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
 
 # Path to the folder containing the graphics
 GRAPHICS_PATH = os.path.abspath('../graphics')
@@ -27,9 +27,9 @@ GRAPHICS_PATH = os.path.abspath('../graphics')
 """
 DEPLOYMENT
 """
-PRODUCTION_S3_BUCKETS = ['apps.npr.org', 'apps2.npr.org']
-STAGING_S3_BUCKETS = ['stage-apps.npr.org']
-ASSETS_S3_BUCKET = 'assets.apps.npr.org'
+PRODUCTION_S3_BUCKETS = ['stlpr-prod.s3-website-us-east-1.amazonaws.com']
+STAGING_S3_BUCKETS = ['stlpr-stg.s3-website-us-east-1.amazonaws.com']
+ASSETS_S3_BUCKET = 'stlpr-assets.s3-website-us-east-1.amazonaws.com'
 
 # These variables will be set at runtime. See configure_targets() below
 S3_BUCKETS = []
