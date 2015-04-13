45b508492e5eb36691aaa56f207c8b9d05450d54
David Eads <davideads@gmail.com>
2015-04-13 16:10:04 -0400

    add timeout to gunicorn

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -109,7 +109,7 @@ def app(port='8000'):
     """
     Serve app.py.
     """
-    local('gunicorn -b 0.0.0.0:%s --debug --reload app:wsgi_app' % port)
+    local('gunicorn -b 0.0.0.0:%s --timeout 3600 --debug --reload app:wsgi_app' % port)
 
 """
 Deployment
