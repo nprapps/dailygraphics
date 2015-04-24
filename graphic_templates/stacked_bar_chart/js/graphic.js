9cfa9e4cd7c5fb5f0e75fe4f53ef0dd28c3c5de7
Christopher Groskopf <staringmonkey@gmail.com>
2015-04-24 11:43:46 -0400

    Update stacked bar chart template.

--- graphic_templates/stacked_bar_chart/js/graphic.js
+++ graphic_templates/stacked_bar_chart/js/graphic.js
@@ -12,13 +12,6 @@ var LABEL_WIDTH = 55;
 var MOBILE_THRESHOLD = 500;
 
 var colorD3;
-var colors = {
-    'red1': '#6C2315', 'red2': '#A23520', 'red3': '#D8472B', 'red4': '#E27560', 'red5': '#ECA395', 'red6': '#F5D1CA',
-    'orange1': '#714616', 'orange2': '#AA6A21', 'orange3': '#E38D2C', 'orange4': '#EAAA61', 'orange5': '#F1C696', 'orange6': '#F8E2CA',
-    'yellow1': '#77631B', 'yellow2': '#B39429', 'yellow3': '#EFC637', 'yellow4': '#F3D469', 'yellow5': '#F7E39B', 'yellow6': '#FBF1CD',
-    'teal1': '#0B403F', 'teal2': '#11605E', 'teal3': '#17807E', 'teal4': '#51A09E', 'teal5': '#8BC0BF', 'teal6': '#C5DFDF',
-    'blue1': '#28556F', 'blue2': '#3D7FA6', 'blue3': '#51AADE', 'blue4': '#7DBFE6', 'blue5': '#A8D5EF', 'blue6': '#D3EAF7'
-};
 var graphicData = null;
 var isMobile = false;
 
@@ -41,23 +34,23 @@ var onWindowLoaded = function() {
 
             // load the data
             colorD3 = d3.scale.ordinal()
-                .range([ colors['teal3'], colors['orange3'], colors['blue3'], '#ccc' ])
+                .range([ COLORS['teal3'], COLORS['orange3'], COLORS['blue3'], '#ccc' ])
                 .domain(d3.keys(graphicData[0]).filter(function(d) {
                     return d != 'label';
                 }));
-        
+
             graphicData.forEach(function(d) {
                 var x0 = 0;
-                d['values'] = colorD3.domain().map(function(name) { 
-                    return { 
-                        name: name, 
-                        x0: x0, 
-                        x1: x0 += +d[name], 
+                d['values'] = colorD3.domain().map(function(name) {
+                    return {
+                        name: name,
+                        x0: x0,
+                        x1: x0 += +d[name],
                         val: d[name]
                     };
                 });
             });
-        
+
             // setup pym
             pymChild = new pym.Child({
                 renderCallback: render
@@ -77,14 +70,14 @@ var render = function(containerWidth) {
     if (!containerWidth) {
         containerWidth = GRAPHIC_DEFAULT_WIDTH;
     }
-    
+
     // check the container width; set mobile flag if applicable
     if (containerWidth <= MOBILE_THRESHOLD) {
         isMobile = true;
     } else {
         isMobile = false;
     }
-    
+
     // clear out existing graphics
     $graphic.empty();
 
@@ -103,15 +96,15 @@ var render = function(containerWidth) {
  * DRAW THE GRAPH
  */
 var drawGraph = function(graphicWidth) {
-    var margin = { 
-        top: 0, 
-        right: 15, 
-        bottom: 30, 
+    var margin = {
+        top: 0,
+        right: 15,
+        bottom: 30,
         left: (LABEL_WIDTH + LABEL_MARGIN)
     };
     var numBars = graphicData.length;
     var ticksX;
-    
+
     if (isMobile) {
         ticksX = 2;
     } else {
@@ -121,17 +114,17 @@ var drawGraph = function(graphicWidth) {
     // define chart dimensions
     var width = graphicWidth - margin['left'] - margin['right'];
     var height = ((BAR_HEIGHT + BAR_GAP) * numBars);
-    
+
     var x = d3.scale.linear()
         .domain([ 0, 100 ])
         .rangeRound([0, width]);
-        
+
     var y = d3.scale.ordinal()
-        .domain(graphicData.map(function(d) { 
+        .domain(graphicData.map(function(d) {
             return d['values'];
         }))
         .rangeRoundBands([0, height], .1);
-        
+
     // define axis and grid
     var xAxis = d3.svg.axis()
         .scale(x)
@@ -140,9 +133,9 @@ var drawGraph = function(graphicWidth) {
         .tickFormat(function(d) {
             return d + '%';
         });
-        
+
     var x_axis_grid = function() { return xAxis; }
-    
+
     // draw the legend
     var legend = graphicD3.append('ul')
 		.attr('class', 'key')
@@ -164,13 +157,13 @@ var drawGraph = function(graphicWidth) {
     // draw the chart
     var chart = graphicD3.append('div')
         .attr('class', 'chart');
-    
+
     var svg = chart.append('svg')
         .attr('width', width + margin['left'] + margin['right'])
         .attr('height', height + margin['top'] + margin['bottom'])
         .append('g')
         .attr('transform', 'translate(' + margin['left'] + ',' + margin['top'] + ')');
-    
+
     svg.append('g')
         .attr('class', 'x axis')
         .attr('transform', 'translate(0,' + height + ')')
@@ -183,44 +176,44 @@ var drawGraph = function(graphicWidth) {
             .tickSize(-height, 0, 0)
             .tickFormat('')
         );
-        
+
     var group = svg.selectAll('.group')
         .data(graphicData)
         .enter().append('g')
             .attr('class', function(d) {
                 return 'group ' + classify(d['label']);
             })
-            .attr('transform', function(d,i) { 
+            .attr('transform', function(d,i) {
                 return 'translate(0,' + (i * (BAR_HEIGHT + BAR_GAP)) + ')';
             });
-            
+
     group.selectAll('rect')
-        .data(function(d) { 
+        .data(function(d) {
             return d['values'];
         })
         .enter().append('rect')
             .attr('height', BAR_HEIGHT)
-            .attr('x', function(d) { 
-                return x(d['x0']); 
+            .attr('x', function(d) {
+                return x(d['x0']);
             })
-            .attr('width', function(d) { 
+            .attr('width', function(d) {
                 return x(d['x1']) - x(d['x0']);
             })
-            .style('fill', function(d) { 
+            .style('fill', function(d) {
                 return colorD3(d['name']);
             })
-            .attr('class', function(d) { 
+            .attr('class', function(d) {
                 return classify(d['name']);
             });
 
     group.append('g')
         .attr('class', 'value')
         .selectAll('text')
-            .data(function(d) { 
+            .data(function(d) {
                 return d['values'];
             })
         .enter().append('text')
-            .attr('x', function(d, i) { 
+            .attr('x', function(d, i) {
 				return x(d['x1']);
             })
             .attr('dx', function(d, i) {
@@ -230,17 +223,17 @@ var drawGraph = function(graphicWidth) {
             .attr('text-anchor', function(d, i) {
 				return 'end';
             })
-            .attr('class', function(d) { 
+            .attr('class', function(d) {
                 return classify(d['name']);
             })
-            .text(function(d) { 
+            .text(function(d) {
                 if (d['val'] > 0) {
                     var v = d['val'] + '%';
                     return v;
                 }
             });
 
-    
+
     // draw labels for each bar
     var labels = chart.append('ul')
         .attr('class', 'labels')
