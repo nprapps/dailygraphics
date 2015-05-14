5a4320a7e001621fd93d2419dcaa538ddd5d3917
Christopher Groskopf <staringmonkey@gmail.com>
2015-05-14 09:04:08 -0400

    Add custom metrics to analytics tracking. #100 #101.

--- graphic_templates/_base/js/analytics.js
+++ graphic_templates/_base/js/analytics.js
@@ -6,6 +6,10 @@ var ANALYTICS = (function () {
     /*
      * Google Analytics
      */
+    var DIMENSION_PARENT_HOSTNAME = 'dimension1';
+    var DIMENSION_PARENT_URL = 'dimension2';
+    var DIMENSION_PARENT_INITIAL_WIDTH = 'dimension3';
+
     var setupGoogle = function() {
         (function(i,s,o,g,r,a,m) {
             i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -13,15 +17,33 @@ var ANALYTICS = (function () {
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+        ga('create', GOOGLE_ANALYTICS_ACCOUNT_ID, 'auto');
+
         // By default Google tracks the query string, but we want to ignore it.
         var location = window.location.protocol +
             '//' + window.location.hostname +
             window.location.pathname;
 
-        ga('create', GOOGLE_ANALYTICS_ACCOUNT_ID, 'auto');
         ga('set', 'location', location);
         ga('set', 'page', window.location.pathname);
-        ga('send', 'pageview');
+
+        // Custom dimensions & metrics
+        var parentUrl = getParameterByName('parentUrl') || '';
+        var parentHostname = '';
+
+        if (parentUrl) {
+            parentHostname = urlToLocation(parentUrl).hostname;
+        }
+
+        var initialWidth = getParameterByName('initialWidth') || '';
+
+        var customData = {};
+        customData[DIMENSION_PARENT_HOSTNAME] = parentHostname;
+        customData[DIMENSION_PARENT_URL] = parentUrl;
+        customData[DIMENSION_PARENT_INITIAL_WIDTH] = initialWidth;
+
+        // Track pageview
+        ga('send', 'pageview', customData);
      }
 
     /*
