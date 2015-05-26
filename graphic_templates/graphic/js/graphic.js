679e090dec02a250f8052a547442b2c1d6b4a0ab
Christopher Groskopf <staringmonkey@gmail.com>
2015-05-26 18:57:08 -0500

    Update simple graphic template.

--- graphic_templates/graphic/js/graphic.js
+++ graphic_templates/graphic/js/graphic.js
@@ -1,22 +1,49 @@
+// Global config
+var GRAPHIC_DEFAULT_WIDTH = 600;
+var MOBILE_THRESHOLD = 500;
+
+// Global vars
 var pymChild = null;
+var isMobile = false;
 
 /*
- * Render the graphic
+ * Initialize the graphic.
  */
-function render(width) {
-    // TODO: draw your graphic
+var onWindowLoaded = function() {
+    if (Modernizr.svg) {
+        pymChild = new pym.Child({
+            renderCallback: render
+        });
+    } else {
+        pymChild = new pym.Child({});
+    }
+}
 
+/*
+ * Render the graphic.
+ */
+var render = function(containerWidth) {
+    if (!containerWidth) {
+        containerWidth = GRAPHIC_DEFAULT_WIDTH;
+    }
+
+    if (containerWidth <= MOBILE_THRESHOLD) {
+        isMobile = true;
+    } else {
+        isMobile = false;
+    }
+
+    // Draw something here
+
+    // Update iframe
     if (pymChild) {
         pymChild.sendHeight();
     }
 }
 
 /*
- * NB: Use window.load instead of document.ready
- * to ensure all images have loaded
+ * Initially load the graphic
+ * (NB: Use window.load instead of document.ready
+ * to ensure all images have loaded)
  */
-$(window).load(function() {
-    pymChild = new pym.Child({
-        renderCallback: render
-    });
-})
+window.onload = onWindowLoaded;
