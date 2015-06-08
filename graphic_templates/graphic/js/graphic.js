d8a0c4f4662eb7aa3a9e3f4e483d8c395ff2d470
Christopher Groskopf <staringmonkey@gmail.com>
2015-06-08 10:50:06 -0500

    Add D3 boilerplate to graphic.

--- graphic_templates/graphic/js/graphic.js
+++ graphic_templates/graphic/js/graphic.js
@@ -33,12 +33,49 @@ var render = function(containerWidth) {
         isMobile = false;
     }
 
-    // Draw something here
+    // Render the chart!
+    // renderGraphic({
+    //     container: '#graphic',
+    //     width: containerWidth,
+    //     data: []
+    // });
 
     // Update iframe
     if (pymChild) {
         pymChild.sendHeight();
     }
+}
+
+/*
+ * Render a graphic.
+ */
+var renderGraphic = function(config) {
+    var aspectWidth = 4;
+    var aspectHeight = 3;
+
+    var margins = {
+        top: 0,
+        right: 15,
+        bottom: 20,
+        left: 15
+    };
+
+    // Calculate actual chart dimensions
+    var chartWidth = config.width - margins.left - margins.right;
+    var chartHeight = Math.ceil((config.width * aspectHeight) / aspectWidth) - margins.top - margins.bottom;
+
+    // Clear existing graphic (for redraw)
+    var containerElement = d3.select(config.container);
+    containerElement.html('');
+
+    // Create container
+    var chartElement = containerElement.append('svg')
+        .attr('width', chartWidth + margins.left + margins.right)
+        .attr('height', chartHeight + margins.top + margins.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+
+    // Draw here!
 }
 
 /*
