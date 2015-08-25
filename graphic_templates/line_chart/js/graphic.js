1f4761169232bfe6e6e32c7f26f7801da73a35c9
Brent Jones <bjones@stlpublicradio.org>
2015-08-25 15:44:55 -0500

    replace missing bracket in line chart template

--- graphic_templates/line_chart/js/graphic.js
+++ graphic_templates/line_chart/js/graphic.js
@@ -3,7 +3,7 @@ var GRAPHIC_DEFAULT_WIDTH = 600;
 var MOBILE_THRESHOLD = 500;
 
 var colors = {
-    'brown': '#6b6256','tan': '#a5a585','ltgreen': '#70a99a','green': '#449970','dkgreen': '#31716e','ltblue': '#55b7d9','blue': '#358fb3','dkblue': '#006c8e','yellow': '#f1bb4f','orange': '#f6883e','tangerine': '#e8604d','red': '#cc203b','pink': '#c72068','maroon': '#8c1b52','purple': '#571751'
+    'brown': '#6b6256','tan': '#a5a585','ltgreen': '#70a99a','green': '#449970','dkgreen': '#31716e','ltblue': '#55b7d9','blue': '#358fb3','dkblue': '#006c8e','yellow': '#f1bb4f','orange': '#f6883e','tangerine': '#e8604d','red': '#cc203b','pink': '#c72068','maroon': '#8c1b52','purple': '#571751' }
 
 // Global vars
 var pymChild = null;
