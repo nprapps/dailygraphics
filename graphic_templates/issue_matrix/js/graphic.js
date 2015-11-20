79dbca2777844c2581604d126b9379abf4b9de86
Alyson Hurt <ahurt@npr.org>
2015-11-20 16:54:15 -0500

    new template: issue matrix

---
+++ graphic_templates/issue_matrix/js/graphic.js
@@ -0,0 +1,17 @@
+/*
+ * Initialize the graphic.
+ */
+var onWindowLoaded = function() {
+    // Uncomment to enable column sorting
+    // var tablesort = new Tablesort(document.getElementById('state-table'));
+
+    pymChild = new pym.Child({});
+}
+
+
+/*
+ * Initially load the graphic
+ * (NB: Use window.load instead of document.ready
+ * to ensure all images have loaded)
+ */
+window.onload = onWindowLoaded;
