7f1a410f4df4372520ef8feaba7b8887e6014ac2
Katie Park <katie.e.park@gmail.com>
2016-05-02 17:32:28 -0400

    Add wrapText to helper js

--- graphic_templates/_base/js/helpers.js
+++ graphic_templates/_base/js/helpers.js
@@ -70,3 +70,52 @@ var urlToLocation = function(url) {
     a.href = url;
     return a;
 }
+
+/*
+ * Wrap a block of SVG text to a given width
+ * adapted from http://bl.ocks.org/mbostock/7555321
+ */
+var wrapText = function(texts, width, lineHeight) {
+    texts.each(function() {
+        var text = d3.select(this);
+        var words = text.text().split(/\s+/).reverse();
+
+        var word = null;
+        var line = [];
+        var lineNumber = 0;
+
+        var x = text.attr('x');
+        var y = text.attr('y');
+
+        var dx = text.attr('dx') ? parseFloat(text.attr('dx')) : 0;
+        var dy = text.attr('dy') ? parseFloat(text.attr('dy')) : 0;
+
+        var tspan = text.text(null)
+            .append('tspan')
+            .attr('x', x)
+            .attr('y', y)
+            .attr('dx', dx + 'px')
+            .attr('dy', dy + 'px');
+
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(' '));
+
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(' '));
+                line = [word];
+
+                lineNumber += 1;
+
+                tspan = text.append('tspan')
+                    .attr('x', x)
+                    .attr('y', y)
+                    .attr('dx', dx + 'px')
+                    .attr('dy', lineNumber * lineHeight)
+                    .attr('text-anchor', 'begin')
+                    .text(word);
+            }
+        }
+    });
+}
