23d567acb09d7a41e0f2783b5a4751bf05808a3f
jjelosua <jelosua@npr.org>
2016-07-12 11:47:38 -0400

    remove carebot specifics

--- graphic_templates/newsletter/js/graphic.js
+++ graphic_templates/newsletter/js/graphic.js
@@ -10,14 +10,6 @@ var onWindowLoaded = function() {
     pymChild = new pym.Child({});
 
     initForm();
-
-    pymChild.onMessage('on-screen', function(bucket) {
-        ANALYTICS.trackEvent('on-screen', bucket);
-    });
-    pymChild.onMessage('scroll-depth', function(data) {
-        data = JSON.parse(data);
-        ANALYTICS.trackEvent('scroll-depth', data.percent, data.seconds);
-    });
 }
 
 var sendPymHeight = function() {
