f1384a906a07ed4f9e654eb2d677219fb5c28089
Alyson Hurt <ahurt@npr.org>
2016-10-06 14:47:03 -0400

    update world map template to new pym/carebot conventions

--- graphic_templates/world_map/js/graphic.js
+++ graphic_templates/world_map/js/graphic.js
@@ -16,6 +16,14 @@ var onWindowLoaded = function() {
         loadGeoData();
     } else {
         pymChild = new pym.Child({});
+
+        pymChild.onMessage('on-screen', function(bucket) {
+            ANALYTICS.trackEvent('on-screen', bucket);
+        });
+        pymChild.onMessage('scroll-depth', function(data) {
+            data = JSON.parse(data);
+            ANALYTICS.trackEvent('scroll-depth', data.percent, data.seconds);
+        });
     }
 }
 
@@ -47,6 +55,14 @@ var loadGeoData = function() {
 
         pymChild = new pym.Child({
             renderCallback: render
+        });
+
+        pymChild.onMessage('on-screen', function(bucket) {
+            ANALYTICS.trackEvent('on-screen', bucket);
+        });
+        pymChild.onMessage('scroll-depth', function(data) {
+            data = JSON.parse(data);
+            ANALYTICS.trackEvent('scroll-depth', data.percent, data.seconds);
         });
     });
 }
