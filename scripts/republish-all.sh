460041c0a742d4dc4a06d2f55c7f710b38202a98
jjelosua <jelosua@npr.org>
2017-03-23 10:57:20 -0400

    stop republishing if an error occurs

--- scripts/republish-all.sh
+++ scripts/republish-all.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # cat $1 | parallel scripts/republish.sh $2 {}
-cat $1 | while read line; do fab $2 deploy:"$line"; sleep 0.5; done
+cat $1 | while read line
+do
+    fab $2 deploy:"$line"
+    if [ $? -ne 0 ]
+    then
+        break
+    fi
+    sleep 10
+done
