a82299c7b60c51937ec0576778ccfde49027cb02
Alyson Hurt <ahurt@npr.org>
2017-09-26 18:42:41 -0400

    ai2html_graphic: pare down JS libraries included. closes #240

--- graphic_templates/ai2html_graphic/js/graphic.js
+++ graphic_templates/ai2html_graphic/js/graphic.js
@@ -24,28 +24,28 @@ var onWindowLoaded = function() {
  */
 var artboardResizer = function() {
     // only want one resizer on the page
-    if (document.documentElement.className.indexOf("g-resizer-v3-init") > -1) return;
-    document.documentElement.className += " g-resizer-v3-init";
+    if (document.documentElement.className.indexOf('g-resizer-v3-init') > -1) return;
+    document.documentElement.className += ' g-resizer-v3-init';
     // require IE9+
-    if (!("querySelector" in document)) return;
+    if (!('querySelector' in document)) return;
     function resizer() {
-        var elements = Array.prototype.slice.call(document.querySelectorAll(".g-artboard[data-min-width]")),
+        var elements = Array.prototype.slice.call(document.querySelectorAll('.g-artboard[data-min-width]')),
             widthById = {};
         elements.forEach(function(el) {
             var parent = el.parentNode,
                 width = widthById[parent.id] || parent.getBoundingClientRect().width,
-                minwidth = el.getAttribute("data-min-width"),
-                maxwidth = el.getAttribute("data-max-width");
+                minwidth = el.getAttribute('data-min-width'),
+                maxwidth = el.getAttribute('data-max-width');
             widthById[parent.id] = width;
             if (+minwidth <= width && (+maxwidth >= width || maxwidth === null)) {
-                el.style.display = "block";
+                el.style.display = 'block';
             } else {
-                el.style.display = "none";
+                el.style.display = 'none';
             }
         });
         try {
             if (window.parent && window.parent.$) {
-                window.parent.$("body").trigger("resizedcontent", [window]);
+                window.parent.$('body').trigger('resizedcontent', [window]);
             }
             if (window.require) {
                 require(['foundation/main'], function() {
