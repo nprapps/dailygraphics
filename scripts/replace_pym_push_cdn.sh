e163b9521e9e4d323e39b2129ab30e81ea3b7844
Juan Elosua <jelosua@npr.org>
2018-02-22 20:54:32 -0500

    small improvements to shell script

--- scripts/replace_pym_push_cdn.sh
+++ scripts/replace_pym_push_cdn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gsed -E -i.bak "s/\s*\{\{\s*JS.push\('js\/lib\/pym.js'\)\s*\}\}\s*/<script type=\"text\/javascript\" src=\"https:\/\/pym.nprapps.org\/pym.v1.min.js\"><\/script>/g" "$1"
+gsed -E -i.bak "s/\s*\{\{\s*JS.push\([\"']js\/lib\/pym.js[\"']\)\s*\}\}\s*/<script type=\"text\/javascript\" src=\"https:\/\/pym.nprapps.org\/pym.v1.min.js\"><\/script>/g" "$1"
