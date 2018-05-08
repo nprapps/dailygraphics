d5a6445bc160c70bc94994f5dc78a08daa5c9862
Alyson Hurt <ahurt@npr.org>
2018-05-08 13:24:27 -0400

    move homepage param check from base.js to helpers.js

--- graphic_templates/_base/js/base.js
+++ graphic_templates/_base/js/base.js
@@ -5,17 +5,6 @@
 // Global config
 var DEFAULT_WIDTH = 600;
 var MOBILE_THRESHOLD = 500;
-var isHomepage = false;
-
-
-// check if this is on the homepage (if someone clicked the
-// "This code will be embedded on the NPR homepage."
-// checkbox when pulling the embed code.)
-if (getParameterByName('mode') == 'hp') {
-    d3.select('body').classed('hp', true);
-    isHomepage = true;
-}
-
 
 // D3 formatters
 var fmtComma = d3.format(',');
