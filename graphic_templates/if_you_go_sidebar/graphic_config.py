72dbd8b7b104e98cfafffbf85a72e2dbea7568a8
Petrin <@stlpublicradio.org>
2018-09-04 15:00:34 -0500

    adds if_you_go folder, unconfigured

---
+++ graphic_templates/if_you_go_sidebar/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1mFUbDPWqqw6V5De3wMX7z11LG1jttQSLs1RRxRP4OOU'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
